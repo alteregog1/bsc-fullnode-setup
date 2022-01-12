e0478cdcc9d96f627c38db69983ac98bbf33885d
alteregog1 <88210725+alteregog1@users.noreply.github.com>
2022-01-12 16:51:46 +0700

    Create step1.sh

---
+++ step1.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+echo "========================================================="
+echo "=       Updating and installing requirement             ="
+echo "========================================================="
+apt-get update
+apt-get install unzip -y
+apt-get install lz4 -y
+
+geth_linux="https://github.com/binance-chain/bsc/releases/download/v1.1.7/geth_linux"
+mainnet="https://github.com/binance-chain/bsc/releases/download/v1.1.7/mainnet.zip"
+
+
+echo "========================================================="
+echo "=    1. Downloading GETH LINUX                          ="
+echo "========================================================="
+wget $geth_linux
+echo "========================================================="
+echo "=    2. Downloading genesis.json and config.toml        ="
+echo "========================================================="
+wget $mainnet
+echo "========================================================="
+echo "=    3. UNZIPING MAINNET                                ="
+echo "========================================================="
+unzip mainnet.zip
+rm ./mainnet.zip
+echo "========================================================="
+echo "=    4. Initiliaze Genesis Node                         ="
+echo "========================================================="
+./geth_linux --datadir ./node init ./genesis.json
+echo "========================================================="
+echo "=    5. DOWNLOADING SNAPSHOT                            ="
+echo "=    Downloading on Background                          ="
+echo "========================================================="
+echo "========================================================="
+echo "=    Please see this link to download your snapshot     ="
+echo "=    https://github.com/binance-chain/bsc-snapshots     ="
+echo "========================================================="
+echo "Input your snapshot endpoint: "
+read eu_endpoint 
+nohup wget -bqc -O geth.tar.lz4 $eu_endpoint &
